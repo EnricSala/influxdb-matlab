6746a9a080141d973f76b8bd890fe079ffe9074b
Enric Sala <sinh32.enric@gmail.com>
2020-10-31 14:51:14 +0100

    Increase build verbosity

--- run_tests.m
+++ run_tests.m
@@ -1,7 +1,27 @@
 clear; clc;
 
+import matlab.unittest.TestRunner;
+import matlab.unittest.plugins.TestRunProgressPlugin;
+
 % Add the library to the path
 addpath('influxdb-client');
 
+% Check if running on a CI environment
+CI = ~isempty(getenv('CI'));
+
+% Initialize a test runner
+runner = TestRunner.withTextOutput;
+if CI
+    runner.addPlugin(TestRunProgressPlugin.withVerbosity(3));
+end
+
 % Run all the tests
-run(testsuite('tests'));
+result = runner.run(testsuite('tests'));
+
+% Display a summary
+if CI
+    Name = {result.Name}';
+    Passed = [result.Passed]';
+    Duration = [result.Duration]';
+    disp(table(Name, Passed, Duration));
+end
