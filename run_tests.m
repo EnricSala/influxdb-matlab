6c4a2f8196259aab530d73cf9d140a926fb7a789
Enric Sala <sinh32.enric@gmail.com>
2020-10-31 15:48:03 +0100

    Configure automated unit tests using Travis CI (#8)

---
+++ run_tests.m
@@ -0,0 +1,29 @@
+clear; clc;
+
+import matlab.unittest.TestRunner;
+import matlab.unittest.plugins.TestRunProgressPlugin;
+
+% Add the library to the path
+addpath('influxdb-client');
+
+% Check if running in a CI environment
+CI = ~isempty(getenv('CI'));
+
+% Initialize a test runner
+runner = TestRunner.withTextOutput;
+if CI
+    runner.addPlugin(TestRunProgressPlugin.withVerbosity(3));
+end
+
+% Run all the tests
+result = runner.run(testsuite('tests'));
+
+% Fail the build if there are failures
+if CI
+    assert(all([result.Passed]), 'There are test failures!');
+    
+    % Display a test summary on success
+    Name = {result.Name}';
+    Duration = [result.Duration]';
+    disp(table(Name, Duration));
+end
